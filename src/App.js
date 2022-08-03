5fe080ca7db80be9198a05144161abe07798524d
Rachel Allen <rallenco13@gmail.com>
2022-08-03 16:27:51 -0600

    refactored to use hooks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { getAllCards } from "./Utils/apiCalls";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cards: [],
-    };
-  }
+function App() {
+  const [cards, setCards] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     // default load data for mount
     getAllCards().then((json) => {
-      console.log(json.cards);
-      this.setState({ cards: json.cards[0] });
+      setCards({ cards: json.cards[0] });
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <main className="App">
-        <h1>Witchcraft Tarot</h1>
-        <section className="tarot-container">
-          <div className="tarot-card">
-            <h2>{this.state.cards.name}</h2>
-            <h2>{this.state.cards.type}</h2>
-            <h2>{this.state.cards.meaning_up}</h2>
-            <h2>{this.state.cards.meaning_rev}</h2>
-            <h2>{this.state.cards.desc}</h2>
-          </div>
-        </section>
-      </main>
-    );
-  }
+  return (
+    <main className="App">
+      <h1>Witchcraft Tarot</h1>
+      <section className="tarot-container">
+        <div className="tarot-card">
+          <h2>{cards.name}</h2>
+          <h2>{cards.type}</h2>
+          <h2>{cards.meaning_up}</h2>
+          <h2>{cards.meaning_rev}</h2>
+          <h2>{cards.desc}</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
-
 export default App;
