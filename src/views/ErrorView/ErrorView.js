fb8be0946340a686a4fb81e58efb8e1b3c09b762
Rachel Allen <rallenco13@gmail.com>
2022-08-05 16:09:55 -0600

    styled the error view

--- src/views/ErrorView/ErrorView.js
+++ src/views/ErrorView/ErrorView.js
@@ -1,11 +1,15 @@
 import * as React from "react";
+import "./ErrorView.scss";
 
-const ExploreView = ({error}) => {
+const ExploreView = ({ error }) => {
   return (
-    <main className="view">
-      <h2>Error View</h2>
-      <h4>{error.message}</h4>
-    </main>
+    <>
+      <main className="view error-view">
+        <h2>Error View</h2>
+        <h4>{error.message}</h4>
+      </main>
+      <div className="error-background"></div>
+    </>
   );
 };
 
