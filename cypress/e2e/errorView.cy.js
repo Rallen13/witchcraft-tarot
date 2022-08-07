ba204153ab8609f3a9e721dc2579e3094241a556
Rachel Allen <rallenco13@gmail.com>
2022-08-07 12:46:59 -0600

    added tests for errorView and all tests passing

--- cypress/e2e/errorView.cy.js
+++ cypress/e2e/errorView.cy.js
@@ -1,5 +1,60 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Error View", () => {
+  it("Should display error for a broken URL", () => {
+    cy.visit("http://localhost:3000/1")
+      .dataCy("error-heading")
+      .contains("I bind you from accessing this page, from doing harm...");
+  });
+
+  it("Should display error message when getAllCards api throws a server error", () => {
+    cy.intercept("GET", "https://rws-cards-api.herokuapp.com/api/v1/cards", {
+      forceNetworkError: true,
+    }).as("getNetworkFailure");
+    cy.visit("http://localhost:3000/explore");
+    cy.wait("@getNetworkFailure")
+      .dataCy("error-heading")
+      .contains("I bind you from accessing this page, from doing harm...");
+  });
+
+  it("Should display error message when getRandomCards api for daily reading throws a server error", () => {
+    cy.intercept(
+      "GET",
+      "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=1",
+      {
+        forceNetworkError: true,
+      }
+    ).as("getNetworkFailure");
+    cy.visit("http://localhost:3000/daily-reading");
+    cy.wait("@getNetworkFailure")
+      .dataCy("error-heading")
+      .contains("I bind you from accessing this page, from doing harm...");
+  });
+
+  it("Should display error message when getRandomCards api for 3-card spread throws a server error", () => {
+    cy.intercept(
+      "GET",
+      "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3",
+      {
+        forceNetworkError: true,
+      }
+    ).as("getNetworkFailure");
+    cy.visit("http://localhost:3000/3-card-spread");
+    cy.wait("@getNetworkFailure")
+      .dataCy("error-heading")
+      .contains("I bind you from accessing this page, from doing harm...");
+  });
+
+  it("Should display error message when getSearchCards api for card details view throws a server error", () => {
+    cy.intercept(
+      "GET",
+      "https://rws-cards-api.herokuapp.com/api/v1/cards/search?q=cupa",
+      {
+        forceNetworkError: true,
+      }
+    ).as("getNetworkFailure");
+    cy.visit("http://localhost:3000/explore/cupa");
+    cy.wait("@getNetworkFailure")
+      .dataCy("error-heading")
+      .contains("I bind you from accessing this page, from doing harm...");
+  });
+
+});
